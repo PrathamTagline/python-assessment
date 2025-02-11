32f128d53041aa439db20e2a2a83cf536f26032f
PrathamTagline <prathamp.tagline@gmail.com>
2025-02-11 14:05:16 +0530

    binary search

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,18 @@
-def binary_search():
-    pass
+def binary_search(givenNumber,numberList):
+    ledingPoint,endingPoint = 0,len(numberList)
+    while ledingPoint < endingPoint:
+        centerPoint = ledingPoint + (endingPoint - ledingPoint) // 2
+        if numberList[centerPoint] == givenNumber:
+            return (centerPoint,numberList[centerPoint])
+        
+        elif numberList[centerPoint] < givenNumber:
+            ledingPoint = centerPoint + 1
+
+        elif numberList[centerPoint] > givenNumber:
+            endingPoint = centerPoint - 1
+    
+    return "element is not available in the list"
+
+
+list1 = [1,2,3,4,5,6,7]
+print(binary_search(-86,list1))
